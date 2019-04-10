e0e089cd241eb4e307bb91830471e77b97ec8904
Vladimir Lukiyanov <vladimir.lukiyanov@me.com>
2019-04-10 16:49:19 +0100

    add docstrings

--- ptsplitter/persona.py
+++ ptsplitter/persona.py
@@ -1,5 +1,5 @@
 from collections import namedtuple
-from typing import Callable, Iterable, Sequence, Hashable
+from typing import Callable, Iterable, Sequence, Hashable, Tuple, List, Dict
 
 import networkx as nx
 
@@ -8,7 +8,18 @@ PersonaNode = namedtuple('PersonaNode', 'node, index')
 
 def create_personas(G: nx.Graph,
                     n: Hashable,
-                    clustering: Callable[[nx.Graph], Iterable[Sequence[Hashable]]] = nx.connected_components):
+                    clustering: Callable[[nx.Graph], Iterable[Sequence[Hashable]]]
+                    ) -> Tuple[List[PersonaNode], Dict[Hashable, PersonaNode]]:
+    """
+    Given a graph, a node in the graph, and a clustering algorithm, generate the personas for the given node.
+
+    :param G: NetworkX graph object
+    :param n: node in the graph
+    :param clustering: algorithm to cluster node on a graph, a callable taking a graph to an iterable of hashable
+    sequences
+    :return: 2-tuple which holds: personalities, so PersonaNode objects, for the given node; and a remap dictionary
+    which maps every node in the ego-net of n in G to its corresponding PersonaNode of n
+    """
     clusters = clustering(G.subgraph(G.neighbors(n)))
     persona_remap = {}
     personalities = []
@@ -21,7 +32,16 @@ def create_personas(G: nx.Graph,
 
 
 def persona_graph(G: nx.Graph,
-                  clustering: Callable[[nx.Graph], Iterable[Sequence[Hashable]]] = nx.connected_components):
+                  clustering: Callable[[nx.Graph], Iterable[Sequence[Hashable]]] = nx.connected_components
+                  ) -> nx.Graph:
+    """
+    Construct the persona graph of a graph G.
+
+    :param G: input graph
+    :param clustering: algorithm to cluster node on a graph, a callable taking a graph to an iterable of hashable
+    sequences
+    :return: persona graph
+    """
     edge_remap = {}
     for n in G.nodes():
         _, persona_remap = create_personas(G, n, clustering)
