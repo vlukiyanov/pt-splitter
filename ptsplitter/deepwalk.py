e8c3af0588f247a23b4323a6b222f7d4f96eff1f
Vladimir Lukiyanov <vladimir.lukiyanov@me.com>
2019-04-12 11:49:05 +0100

    Further changes to persona and DeepWalk implementations (#3)
    * reword the persona code a little
    
    * add tests for basic deepwalk functions
    
    * add further comments and slightly change the function to select a random node for the walk start

--- ptsplitter/deepwalk.py
+++ ptsplitter/deepwalk.py
@@ -1,8 +1,12 @@
+from multiprocessing import cpu_count
 import random
-from typing import Hashable, Iterator, List, Callable
+from typing import Hashable, Iterator, List, Callable, Dict, Tuple
 
 from cytoolz.itertoolz import take, iterate
+from gensim.models import Word2Vec
 import networkx as nx
+
+# TODO figure out iterator versus iterable for typing
 
 
 def iter_random_walk(G: nx.Graph,
@@ -20,13 +24,54 @@ def iter_random_walk(G: nx.Graph,
     """
     for cur in iterate(lambda x: choice(list(G[x])), n):
         yield cur
-        if len(G[cur]) == 0:
+        if len(G[cur]) == 0:  # is this check needed every iteration?
             return
 
 
 def iter_random_walks(G: nx.Graph,
                       length: int,
                       choice: Callable[[List[Hashable]], Hashable] = random.choice) -> Iterator[List[Hashable]]:
+    """
+    Given an input graph, repeatedly yield random walks of a fixed maximum length starting at random nodes; if
+    the node is disconnected then the walk will consist of the node itself.
+
+    :param G: input graph
+    :param length: maximum length of walk
+    :param choice: choice function to map a list of nodes to a node
+    :return: yields lists of walks
+    """
     while True:
-        for n in G.nodes():
-            yield list(take(length, iter_random_walk(G, n, choice)))
+        yield list(take(length, iter_random_walk(G, choice(list(G.nodes())), choice)))
+
+
+def lookup_tables(G: nx.Graph) -> Tuple[Dict[Hashable, int], Dict[int, Hashable]]:
+    """
+    Given a graph G construct a lookup table between nodes and their integer position in G.nodes() and a reverse
+    lookup as dictionaries.
+
+    :param G: input graph
+    :return: 2-tuple of node->index and index->node lookups
+    """
+    forward = {node: index for index, node in enumerate(G.nodes())}
+    reverse = {forward[node]: node for node in forward}
+    return forward, reverse
+
+
+def initial_deepwalk_embedding(walks: Iterator[List[int]],
+                               forward_lookup: Dict[Hashable, int],
+                               embedding_dimension: int,
+                               min_count: int = 0,
+                               window: int = 10,
+                               workers: int = cpu_count()):  # TODO add type
+    # TODO constructing the list below might use a lot of memory for larger walks
+    model = Word2Vec(
+        [[str(forward_lookup[node]) for node in walk] for walk in walks],
+        size=embedding_dimension,
+        window=window,
+        min_count=min_count,
+        sg=1,  # use skip-gram
+        hs=1,  # use hierarchical softmax
+        workers=workers,
+        iter=1  # TODO figure out iter setting
+    )
+    return {node: model[str(forward_lookup[node])] for node in forward_lookup}
