109514f2f44409f0d2c7ca8537d63df974db4996
Vladimir Lukiyanov <vladimir.lukiyanov@me.com>
2019-04-12 11:36:51 +0100

    add further comments and slightly change the function to select a random node for the walk start

--- ptsplitter/deepwalk.py
+++ ptsplitter/deepwalk.py
@@ -24,19 +24,34 @@ def iter_random_walk(G: nx.Graph,
     """
     for cur in iterate(lambda x: choice(list(G[x])), n):
         yield cur
-        if len(G[cur]) == 0:
+        if len(G[cur]) == 0:  # is this check needed every iteration?
             return
 
 
 def iter_random_walks(G: nx.Graph,
                       length: int,
                       choice: Callable[[List[Hashable]], Hashable] = random.choice) -> Iterator[List[Hashable]]:
+    """
+    Given an input graph, repeatedly yield random walks of a fixed maximum length starting at random nodes; if
+    the node is disconnected then the walk will consist of the node itself.
+
+    :param G: input graph
+    :param length: maximum length of walk
+    :param choice: choice function to map a list of nodes to a node
+    :return: yields lists of walks
+    """
     while True:
-        for n in G.nodes():
-            yield list(take(length, iter_random_walk(G, n, choice)))
+        yield list(take(length, iter_random_walk(G, choice(list(G.nodes())), choice)))
 
 
 def lookup_tables(G: nx.Graph) -> Tuple[Dict[Hashable, int], Dict[int, Hashable]]:
+    """
+    Given a graph G construct a lookup table between nodes and their integer position in G.nodes() and a reverse
+    lookup as dictionaries.
+
+    :param G: input graph
+    :return: 2-tuple of node->index and index->node lookups
+    """
     forward = {node: index for index, node in enumerate(G.nodes())}
     reverse = {forward[node]: node for node in forward}
     return forward, reverse
@@ -48,6 +63,7 @@ def initial_deepwalk_embedding(walks: Iterator[List[int]],
                                min_count: int = 0,
                                window: int = 10,
                                workers: int = cpu_count()):  # TODO add type
+    # TODO constructing the list below might use a lot of memory for larger walks
     model = Word2Vec(
         [[str(forward_lookup[node]) for node in walk] for walk in walks],
         size=embedding_dimension,
@@ -56,6 +72,6 @@ def initial_deepwalk_embedding(walks: Iterator[List[int]],
         sg=1,  # use skip-gram
         hs=1,  # use hierarchical softmax
         workers=workers,
-        iter=1  # TODO
+        iter=1  # TODO figure out iter setting
     )
     return {node: model[str(forward_lookup[node])] for node in forward_lookup}
