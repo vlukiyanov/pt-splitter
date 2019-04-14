b6380f91bb6deedfda29b332e0d58ddf865e3e1b
Vladimir Lukiyanov <vladimir.lukiyanov@me.com>
2019-04-14 14:25:11 +0100

    Some utils for transition matrix based random walks (#5)
    * move the if check outside the loop
    
    * add skeleton utils for transition matrix based random walks

---
+++ ptsplitter/utils.py
@@ -0,0 +1,18 @@
+import networkx as nx
+import numpy as np
+import scipy as sp
+from scipy.sparse import identity, spdiags
+
+
+def basis_vector(size, i, **kwargs):
+    v = np.zeros(size, **kwargs)
+    v[i] = 1
+    return v
+
+
+def transition_matrix(G):
+    M = nx.to_scipy_sparse_matrix(G, dtype=float)  # TODO extra options
+    n, m = M.shape
+    DI = spdiags(1.0 / sp.array(M.sum(axis=1).flat), [0], n, n)
+    return (DI * M).transpose().tocsr()
+
