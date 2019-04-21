b1a5ee7055669724d6f4a90abd0e48daaebda62f
Vladimir Lukiyanov <vladimir.lukiyanov@me.com>
2019-04-22 00:54:18 +0100

    Sketchy implementation using adaptive softmax (#6)
    * change the order of the imports to be alphabetical
    
    * try and fix a test failure issue with Travis
    
    * add functions to handle getting out the initial embedding matrix
    
    * initial placeholders for splitter model
    
    * add initial untested splitter model using adaptive softmax

--- ptsplitter/splitter.py
+++ ptsplitter/splitter.py
@@ -1,6 +1,54 @@
+import torch
 import torch.nn as nn
 
 
-class Splitter(nn.Module):
-    def __init__(self):
-        super(Splitter, self).__init__()
+class SplitterEmbedding(nn.Module):
+    def __init__(self,
+                 node_count: int,
+                 ego_node_count: int,
+                 embedding_dimension: int,
+                 lamb: float = 0.1,
+                 initial_embedding=None) -> None:
+        super(SplitterEmbedding, self).__init__()
+        self.node_count = node_count
+        self.ego_node_count = ego_node_count
+        self.embedding_dimension = embedding_dimension
+        self.lamb = lamb
+        # embedding layers
+        self.persona_embedding = nn.Embedding(
+            num_embeddings=self.ego_node_count,
+            embedding_dim=self.embedding_dimension,
+            padding_idx=0
+        )
+        self.embedding = nn.Embedding(
+            num_embeddings=self.node_count,
+            embedding_dim=self.embedding_dimension,
+            padding_idx=0
+        )
+        if initial_embedding is None:
+            initial_embedding = torch.ones(node_count, embedding_dimension)
+        else:
+            initial_embedding = torch.from_numpy(initial_embedding)
+        self.persona_embedding.weight.data = torch.nn.Parameter(initial_embedding, requires_grad=False)
+        self.embedding.weight.data = torch.nn.Parameter(initial_embedding)
+        # adaptive softmax layers
+        self.predict_persona_embedding = torch.nn.AdaptiveLogSoftmaxWithLoss(
+            in_features=embedding_dimension,
+            n_classes=ego_node_count,
+            cutoffs=[round(node_count/10), 2*round(node_count/10), 3*round(node_count/10)]
+        )
+        self.predict_embedding = torch.nn.AdaptiveLogSoftmaxWithLoss(
+            in_features=embedding_dimension,
+            n_classes=node_count,
+            cutoffs=[round(node_count/10), 2*round(node_count/10), 3*round(node_count/10)]
+        )
+
+    def loss(self, persona_batch, pure_node_batch, context_node_batch):
+        # main loss
+        main_loss = self.predict_persona_embedding(self.persona_embedding(persona_batch), context_node_batch)
+        # regularisation loss
+        regularisation_loss = self.predict_embedding(self.persona_embedding(persona_batch), pure_node_batch)
+        return main_loss.loss + self.lamb*regularisation_loss.loss
+
+    def forward(self, persona_batch):
+        return self.persona_embedding(persona_batch)
---
+++ tests/test_splitter.py
@@ -0,0 +1,54 @@
+from cytoolz.itertoolz import take
+import networkx as nx
+import torch
+
+from ptsplitter.deepwalk import to_embedding_matrix, iter_random_walks, lookup_tables, initial_deepwalk_embedding
+from ptsplitter.splitter import SplitterEmbedding
+
+
+graph_abcd = nx.from_edgelist([
+    ('a', 'b'),
+    ('b', 'c'),
+    ('c', 'd')
+])
+
+
+def test_embedding_basic():
+    embedding = SplitterEmbedding(
+        node_count=10,
+        ego_node_count=15,
+        embedding_dimension=100
+    )
+    persona_batch = torch.ones(5).long()
+    output = embedding(persona_batch)
+    assert output.shape == (5, 100)
+
+
+def test_embedding_graph():
+    forward, reverse = lookup_tables(graph_abcd)
+    walks = take(10, iter_random_walks(graph_abcd, 5))
+    node_embeddings = initial_deepwalk_embedding(walks, forward, 100)
+    initial_embedding = to_embedding_matrix(node_embeddings, 100, reverse)
+    embedding = SplitterEmbedding(
+        node_count=10,
+        ego_node_count=15,
+        embedding_dimension=100,
+        initial_embedding=initial_embedding
+    )
+    persona_batch = torch.ones(5).long()
+    output = embedding(persona_batch)
+    assert output.shape == (5, 100)
+
+
+def test_loss_basic():
+    embedding = SplitterEmbedding(
+        node_count=10,
+        ego_node_count=15,
+        embedding_dimension=100,
+    )
+    persona_batch = torch.ones(5).long()
+    pure_node_batch = torch.tensor([0, 1, 2, 0, 1])
+    context_node_batch = torch.tensor([0, 2, 1, 4, 2])
+    output = embedding.loss(persona_batch, pure_node_batch, context_node_batch)
+    assert output.shape == tuple()
+    output.backward()
